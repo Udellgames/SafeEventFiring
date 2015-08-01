a345961b08a38d41cb3686fc6142154772358a31
nick udell <nick@udellgames.com>
2015-08-01 01:59:41 +0100

    Added support for PropertyChanged events.

---
+++ SafeEventHandlers/PropertyChangedEventHandlerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+/// <summary>
+/// Extension methods for PropertyChangedEventHandlers
+/// </summary>
+public static class PropertyChangedEventHandlerExtensions
+{
+    /// <summary>
+    /// Raises an event thread-safely if the event has subscribers.
+    /// </summary>
+    /// <param name="handler"> The event handler to raise. </param>
+    /// <param name="sender"> The object that sent this event. </param>
+    /// <param name="args"> The event args. </param>
+    [SuppressMessage("Microsoft.Design",
+        "CA1030:UseEventsWhereAppropriate",
+        Justification = "This warning comes up when you use the word `Fire` in a method name. This method specifically raises events, and so does not need changing.")]
+    [DebuggerHidden]
+    public static void Fire(this PropertyChangedEventHandler handler, object sender, PropertyChangedEventArgs args)
+    {
+        if (handler != null)
+        {
+            handler(sender, args);
+        }
+    }
+}
---
+++ SafeEventHandlersTests/PropertyChangedEventHandlerExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.ComponentModel;
+
+namespace Tests
+{
+    [TestFixture]
+    public class PropertyChangedEventHandlerExtensionsTests
+    {
+
+        private event PropertyChangedEventHandler handler;
+
+        [Test]
+        public void Fire_Test()
+        {
+            var expectedArgs = new PropertyChangedEventArgs("foo");
+
+            var eventFired = false;
+
+            PropertyChangedEventHandler action = (o, e) =>
+            {
+                Assert.That(o, Is.SameAs(this));
+                Assert.That(e, Is.EqualTo(expectedArgs));
+
+                eventFired = true;
+            };
+
+            handler += action;
+
+            handler.Fire(this, expectedArgs);
+
+            Assert.That(eventFired);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (handler != null)
+            {
+                foreach (var invocation in handler.GetInvocationList())
+                {
+                    handler -= (PropertyChangedEventHandler)invocation;
+                }
+            }
+        }
+    }
+}
